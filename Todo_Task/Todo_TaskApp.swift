72971302c9f0ba8dbf68abc31ae9ea6b092ae6f1
Mustafa KILINÇ <mustafakilinc429@gmail.com>
2023-04-15 13:57:29 +0300

    Initial Commit

---
+++ Todo_Task/Todo_TaskApp.swift
@@ -0,0 +1,20 @@
+//
+//  Todo_TaskApp.swift
+//  Todo_Task
+//
+//  Created by Mustafa Kılınç on 15.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct Todo_TaskApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
